651fb6373ec9891725d80324ce05a504de2a548d
Mocsonoky Attila <kismocsy@gmail.com>
2020-11-16 08:59:23 +0100

    XSS

--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -4,6 +4,9 @@
     <ul v-for="(recipePost, index) in recipePosts" :key="index" class="articles">
       <nuxt-link class="article article--clickable" :to="`recipes/${recipePost.slug}`">
         <h3 class="article-title">{{ recipePost.title }}</h3>
+        <div v-if="recipePost.gallery">
+          <img v-for="image in recipePost.gallery" class="image" :key="image.id" :src="image" />
+        </div>
         <div class="mt-4 mb-2">
           <h2 class="inline py-1 px-2 mr-1 bg-accent text-white font-medium rounded-sm dark:bg-accent">
             {{ recipePost.recipe_type }}
